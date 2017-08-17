b7f7d8ad880f2ab59970174b64fe8f8f5e2baa6b
redheatwei <qjyyn@qq.com>
2017-08-17 14:30:49 +0800

    sync by bash

--- sync-by-bash.py
+++ sync-by-bash.py
@@ -17,13 +17,16 @@ class HttpRequest(object):
         # else:
         #     req = urllib2.Request(url)
         res = urllib2.urlopen(req)
-        # res = res.read()
+        res = res.read()
+        s = json.dumps(res,__dict__)
+        print s
+        print type(s)
         print(res)
     def post_request(self,url,textmod=None,header_dict=None):
         textmod = json.dumps(textmod)
         req = urllib2.Request(url=url, data=textmod,headers=header_dict)
         res = urllib2.urlopen(req)
-        # res = res.read()
+        res = res.read()
         print(res)
 class SyncFromRemote(object):
     def __init__(self,private_token):
