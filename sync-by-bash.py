a47e99086b655e2534ee1ce07d2ded47b70925f0
redheatwei <qjyyn@qq.com>
2017-08-17 14:15:08 +0800

    sync by bash

---
+++ sync-by-bash.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+'''
+Created on '2017/8/17 10:40'
+Email: qjyyn@qq.com
+@author: Redheat
+'''
+import  urllib,urllib2,json
+class HttpRequest(object):
+    def get_request(self,url,textmod=None,header_dict=None):
+        if textmod:
+            textmod = urllib.urlencode(textmod)
+            url = '%s%s%s' % (url, '?', textmod)
+        req = urllib2.Request(url, headers=header_dict)
+        # if header_dict:
+        #     req = urllib2.Request(url,headers=header_dict)
+        # else:
+        #     req = urllib2.Request(url)
+        res = urllib2.urlopen(req)
+        res = res.read()
+        print(res)
+    def post_request(self,url,textmod=None,header_dict=None):
+        textmod = json.dumps(textmod)
+        req = urllib2.Request(url=url, data=textmod,headers=header_dict)
+        res = urllib2.urlopen(req)
+        res = res.read()
+        print(res)
+class SyncFromRemote(object):
+    def __init__(self,private_token):
+        # self.group = group
+        # self.project = project
+        self.url = "http://10.100.218.203/api/v3/groups"
+        self.http_request = HttpRequest()
+        self.private_token = private_token
+    def get_all_group(self):
+        header_dict = {"PRIVATE-TOKEN":self.private_token}
+        self.http_request.get_request(self.url,header_dict=header_dict)
+
+sync = SyncFromRemote("vXb3ysPaQ6naPUF9z-FM")
+
+
