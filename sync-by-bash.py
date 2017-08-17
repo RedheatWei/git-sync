e47527c14256b2fa0efc1961a47513fa8e7a567a
redheatwei <qjyyn@qq.com>
2017-08-17 19:00:30 +0800

    fix bug

--- sync-by-bash.py
+++ sync-by-bash.py
@@ -150,7 +150,7 @@ sync = SyncFromRemote("vXb3ysPaQ6naPUF9z-FM")
 
 for remote in config:
     group_name = "/".split(config[remote]["fetch"])[-1]
-    project_fetch = config[remote]["fetch"]
+    project_fetch = "/".split('@'.split(config[remote]["fetch"])[-1])[0]
     for project_name in config[remote]["project"]:
         sync.down_remote_mirror(group_name,project_name,project_fetch)
         sync.push_mirror_to_local(group_name,project_name)
