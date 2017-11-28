4b68becd711685f121b1d793a64848bc566e53a3
weipeng4 <weipeng4@lenovo.com>
2017-11-28 11:03:56 +0800

    change wait time

--- sync-no-change.py
+++ sync-no-change.py
@@ -252,7 +252,7 @@ def sync_code(config,sync):
             if sync.down_remote_mirror(group_name,project_name,project_fetch) ==0:
                 sync.push_mirror_to_local(group_name,project_name,project_fetch)
             sync.update_mirror(group_name,project_name,project_fetch)
-            time.sleep(7)
+            time.sleep(10)
 
 def change_local(need_change,xml_file,repo):
     local_code = CloneToLocal(need_change,xml_file,repo)
